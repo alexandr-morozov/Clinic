e96b34078fdc28400f97a534f35bfe19989d71ce
Alexander Morozov <amorozovo@novapulsar.com>
2021-10-19 04:54:31 +0300

    Add found function by name for doctors and patients

--- function.php
+++ function.php
@@ -7,18 +7,20 @@ function printMenu()
     echo "2. Add new doctors\n";
     echo "3. Print info about all patients\n";
     echo "4. Print info about all doctors\n";
-    echo "5. Exit\n";
+    echo "5. Print info about one patient\n";
+    echo "6. Print info about one doctor\n";
+    echo "7. Exit\n";
     echo ">";
 }
 
 function menu($clinic)
 {
     $chooseUser=0;
-    while ($chooseUser != 5)
+    while ($chooseUser != 7)
     {
         printMenu();
         $chooseUser = setNumber();
-        if ($chooseUser>5 or $chooseUser<1)
+        if ($chooseUser>7 or $chooseUser<1)
         {
             echo"Error input, try again: ";
         }
@@ -35,6 +37,12 @@ function menu($clinic)
                 break;
             case 4:
                 $clinic -> printListDoctor();
+                break;
+            case 5:
+                $clinic -> getInfoPatientByName();
+                break;
+            case 6:
+                $clinic -> getInfoDoctorByName();
                 break;
         }
     }
@@ -53,5 +61,24 @@ function setNumber()
         } else {
             echo "\nError Input, try again: \n";
         }
+    }
+}
+
+function getInfoByName($listElements, $countElements)
+{
+    echo"Input name: ";
+    $keyName = readline();
+    $countEqual = 0;
+    for ($i = 0; $i < $countElements; $i++)
+    {
+        if ($keyName == $listElements[$i]->name)
+        {
+            $countEqual++;
+            $listElements[$i]->printElement();
+        }
+    }
+    if ($countEqual == 0)
+    {
+        echo"Not found info by this name";
     }
 }
